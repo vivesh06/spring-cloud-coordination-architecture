77383c5b0917900e9750a89be673cfdcc54e0865
Vivesh Saladi <vivesh.saladi@momentum.co.za>
2017-12-19 10:29:14 +0200

    hystrix

--- sample-service-discovery/src/main/java/com/learning/SampleServiceDiscoveryApplication.java
+++ sample-service-discovery/src/main/java/com/learning/SampleServiceDiscoveryApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -13,9 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @EnableEurekaClient
 @RestController
 @SpringBootApplication
+@EnableCircuitBreaker
 public class SampleServiceDiscoveryApplication {
 
 	@Bean
@@ -27,11 +31,16 @@ public class SampleServiceDiscoveryApplication {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@HystrixCommand(fallbackMethod="fallback")
 	@RequestMapping("/test")
 	@SuppressWarnings("rawtypes")
 	public String test() {
 		List list = restTemplate.getForObject("http://sample-service/fetch", List.class);
 		return list.toString();
+	}
+	
+	public String fallback() {
+		return "dafult val";
 	}
 	
 	public static void main(String[] args) {
